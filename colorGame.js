a54f5b43434e92bfefa09179e0ff0be717d1359b
Jonathan Friedl <jfreetz2@gmail.com>
2020-04-13 14:34:38 -0500

    got the squares hard coded for dif colors and when you click on them it compares to the hard coded correct color definition

---
+++ colorGame.js
@@ -0,0 +1,29 @@
+var colors =[
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+]
+let squares= document.querySelectorAll(".square");
+let pickedColor= colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+    //add initial colors to square
+    squares[i].style.backgroundColor = colors[i];
+    //add click listeners to squares
+    squares[i].addEventListener('click', function(){
+    //grab color of picked square
+        let clickedColor =this.style.backgroundColor;
+    //compare color to picked color
+    if(clickedColor===pickedColor){
+        alert('Good job');
+    }else{
+        alert('not good');
+    }
+    });
+}
