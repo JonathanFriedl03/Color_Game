2ab3442afd04205ccbd9cf5809cedb95d2666040
Jonathan Friedl <jfreetz2@gmail.com>
2020-04-13 16:00:08 -0500

    added a random function for the square colors to not be hard coded and squares and h1 to change when correct color picked to correct color

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,9 @@
-var colors =[
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors =generateRandomColors(6); 
 let squares= document.querySelectorAll(".square");
-let pickedColor= colors[3];
+let pickedColor= pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
 
 
 colorDisplay.textContent = pickedColor;
@@ -22,9 +16,12 @@ for (let i = 0; i < squares.length; i++) {
     //grab color of picked square
         let clickedColor =this.style.backgroundColor;
     //compare color to picked color
+    console.log(clickedColor, pickedColor);
+    
     if(clickedColor===pickedColor){
         messageDisplay.textContent= 'Correct!';
         changeColors(clickedColor);
+        h1.style.backgroundColor = clickedColor;
     }else{
         //fade out the wrong color picked 
         this.style.backgroundColor ='#232323';
@@ -39,4 +36,29 @@ function changeColors (color){
         squares[i].style.backgroundColor = color;
         
     }
+}
+function pickColor(){
+    let random =Math.floor(Math.random()* colors.length);
+    //access element from array at given index
+    return colors[random];
+}
+
+function generateRandomColors (number){
+    //make an array
+    let arr = [];
+    //add number of random colors to array
+    for (let i = 0; i < number; i++) {
+    //get random color and push into array
+    arr.push(randomColor()); 
+    }        
+    //return that array
+        return arr;
+    
+}
+function randomColor(){
+    //pick a red from 0 - 255 same for greeen and blue
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
